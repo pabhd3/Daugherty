004d0ca66ae541dc2d80e4f355fc7c627e976683
CORPORATE\pab0331 <peter.banko@daugherty.com>
2018-02-05 11:36:55 -0600

    Coding Challenge 02 - added unit tests Added pytest unit tests for challenge 01 and challenge 02

---
+++ Coding-Challenges/02-Feb-02-2018/test_challenge01.py
@@ -0,0 +1,14 @@
+import pytest
+from challenge01 import isogramChecker
+
+def test_one_letter_word():
+    assert isogramChecker("I") == True
+
+def test_consecutive_letters():
+    assert isogramChecker("Apple") == False
+
+def test_non_consecutive_letters():
+    assert isogramChecker("Test") == False
+
+def test_no_repeats():
+    assert isogramChecker("Isogram") == True
