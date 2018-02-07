3453ea572d85abdd09b0351bad9f05e02ad7742e
CORPORATE\pab0331 <peter.banko@daugherty.com>
2018-02-07 13:43:08 -0600

    Exercises 03 - First Spring Boot App - properties Added another GET method to ShipwreckController     - Which fixed an editing issue from last commit Added application.properties file Added application-test.properties file Added application-prod.properties file

--- Daugherty-University/03-Creating-Your-First-Spring-Boot-Application/das-boot/src/main/java/com/boot/controller/ShipwreckController.java
+++ Daugherty-University/03-Creating-Your-First-Spring-Boot-Application/das-boot/src/main/java/com/boot/controller/ShipwreckController.java
@@ -19,6 +19,11 @@ public class ShipwreckController {
 		return ShipwreckStub.list();
 	}
 	
+	@RequestMapping(value = "shipwrecks/{id}", method = RequestMethod.GET)
+    public Shipwreck get(@PathVariable Long id) {
+        return ShipwreckStub.get(id);
+    }
+	
 	@RequestMapping(value = "shipwrecks", method = RequestMethod.POST)
 	public Shipwreck create(@RequestBody Shipwreck shipwreck) {
 		return ShipwreckStub.create(shipwreck);
