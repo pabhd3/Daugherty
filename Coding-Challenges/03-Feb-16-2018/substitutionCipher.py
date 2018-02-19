ffc9782f1c6cee91207a724671b774c67889795a
CORPORATE\pab0331 <peter.banko@daugherty.com>
2018-02-19 14:26:55 -0600

    Coding Challenge 03.2 - Substitution Cipher - refactor Moved key generation into its own function keywords that are not isograms can now be used     * repeat letters in the keyword caused an error in KEY generation     previously

--- Coding-Challenges/03-Feb-16-2018/substitutionCipher.py
+++ Coding-Challenges/03-Feb-16-2018/substitutionCipher.py
@@ -1,32 +1,34 @@
-def substitutionEncryption(keyword, plaintext):
-    # Base alphabet used to make our key
+def generateKey(keyword):
     ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    KEY = ""
     # Key used to encrypt the plaintext
-    KEY = keyword.upper()
+    for letter in keyword:
+        if(letter.upper() not in KEY):
+            KEY += letter.upper()
 
     # Create the encryption key using the key word
-    # For each letter in the normal alphabet
     for letter in ALPHABET:
-        # If it wasn't in the keyword
         if(letter not in KEY):
-            # Add it to the key
             KEY += letter
 
-    ciphertext = ""
+    return KEY
 
-    # For each character in the plaintext message
+
+def substitutionEncryption(keyword, plaintext):
+    ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    ciphertext = ""
+    KEY = generateKey(keyword)
+
     for letter in plaintext:
-        # Check if it a letter
         if letter.isalpha():
             # If so, get its index from the original alphabet
             index = ALPHABET.find(letter.upper())
             # Add its substituted letter using the index and
             #     the key we generated
             ciphertext += KEY[index]
-    # We need to format the ciphertext
+
     count = 0
     ciphertextFormatted = ""
-    # For each letter in the cipher text
     for letter in ciphertext:
         # We can only have 5 characters at a time
         if(count % 5 == 0 and count != 0):
@@ -37,43 +39,27 @@ def substitutionEncryption(keyword, plaintext):
         # And a block can only have 250 characters in it
         if(count % 250 == 0 and count != 0):
             ciphertextFormatted += "\n"
-        # Add the letter
         ciphertextFormatted += letter
-        # And increase the count
         count += 1
-    # And we're done
     return ciphertextFormatted
 
+
 def substitutionDecryption(keyword, ciphertext):
-    # Base alphabet used to make our key
     ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    # Key used to encrypt the plaintext
-    KEY = keyword.upper()
-
-    # Create the encryption key using the key word
-    # For each letter in the normal alphabet
-    for letter in ALPHABET:
-        # If it wasn't in the keyword
-        if(letter not in KEY):
-            # Add it to the key
-            KEY += letter
-
     plaintext = ""
-    # For each character in our ciphertext
+    KEY = generateKey(keyword)
+
     for letter in ciphertext:
-        # If the character is a letter
         if(letter.isalpha()):
             # Get its index in the KEY
             index = KEY.find(letter.upper())
             # And add the plaintext using the index and the alphabet
             plaintext += ALPHABET[index]
-    # And we're done
     return plaintext
 
-# Set up the main
 def main():
-    print(substitutionEncryption("zebras", "abcdefghijklmnopqrstuvwxyz"))
-    print(substitutionDecryption("zebras", "ZEBRA SCDFG HIJKL MNOPQ TUVWX\nY"))
+    print(substitutionEncryption("happy", "abcdefghijklmnopqrstuvwxyz"))
+    print(substitutionDecryption("happy", "SUBDE RMATO GLYPH ICFJK NQVWXZ"))
 
 if __name__ == '__main__':
     main()
